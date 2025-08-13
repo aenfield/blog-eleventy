2194906b1818095ae703cf6d4bff2ab58602cd7d
aenfield <aenfield@users.noreply.github.com>
2025-08-13 13:08:33 +0000

    deploy: 501497d0e9be65139d20e49dc649143d05011039

--- dist/nouf0IwDb4.js
+++ dist/nouf0IwDb4.js
@@ -229,4 +229,4 @@ class HeadingAnchors extends HTMLElement {
 HeadingAnchors.register();
 
 export { HeadingAnchors }
-window.location.href = "/blog/2025/08/12/mlb-what-to-watch-on-august-12-2025/";
+window.location.href = "/blog/2025/08/13/mlb-what-to-watch-on-august-13-2025/";
