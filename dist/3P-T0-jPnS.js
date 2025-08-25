e8bb5548e3de735a2525d4733ec636d566a17735
aenfield <aenfield@users.noreply.github.com>
2025-08-25 13:03:03 +0000

    deploy: 3551f3a586e05d928ef025cbebaae851f0e413aa

--- dist/3P-T0-jPnS.js
+++ dist/3P-T0-jPnS.js
@@ -229,4 +229,4 @@ class HeadingAnchors extends HTMLElement {
 HeadingAnchors.register();
 
 export { HeadingAnchors }
-window.location.href = "/blog/2025/08/24/mlb-what-to-watch-on-august-24-2025/";
+window.location.href = "/blog/2025/08/25/mlb-what-to-watch-on-august-25-2025/";
