03b49b6249a98e569c9e620b60066a4c0f6fffc0
aenfield <aenfield@users.noreply.github.com>
2026-08-12 14:44:13 +0000

    deploy: 8d3b905a7de229a36c0409d947873f72e5bfa5d9

--- dist/AnLicd_Pyt.js
+++ dist/AnLicd_Pyt.js
@@ -229,4 +229,4 @@ class HeadingAnchors extends HTMLElement {
 HeadingAnchors.register();
 
 export { HeadingAnchors }
-window.location.href = "/blog/2026/08/11/mlb-what-to-watch-on-august-11-2026/";
+window.location.href = "/blog/2026/08/12/mlb-what-to-watch-on-august-12-2026/";
