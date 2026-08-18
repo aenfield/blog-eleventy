9a40e2ef74ca5c8e2fd00f4fc94cf0cee9d5e3e4
aenfield <aenfield@users.noreply.github.com>
2026-08-18 14:14:28 +0000

    deploy: 1a945b264158e3fd6dea400d37aa81317686bc78

--- dist/L_zc6FbKcA.js
+++ dist/L_zc6FbKcA.js
@@ -229,4 +229,4 @@ class HeadingAnchors extends HTMLElement {
 HeadingAnchors.register();
 
 export { HeadingAnchors }
-window.location.href = "/blog/2026/08/17/mlb-what-to-watch-on-august-17-2026/";
+window.location.href = "/blog/2026/08/18/mlb-what-to-watch-on-august-18-2026/";
