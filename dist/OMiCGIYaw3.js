8fa191d500b9da3b402b2451de5373f0c6c1cd59
aenfield <aenfield@users.noreply.github.com>
2026-08-25 14:28:18 +0000

    deploy: b3e4a11ff172064015391dde570da70f4869bfa8

--- dist/OMiCGIYaw3.js
+++ dist/OMiCGIYaw3.js
@@ -229,4 +229,4 @@ class HeadingAnchors extends HTMLElement {
 HeadingAnchors.register();
 
 export { HeadingAnchors }
-window.location.href = "/blog/2026/08/24/mlb-what-to-watch-on-august-24-2026/";
+window.location.href = "/blog/2026/08/25/mlb-what-to-watch-on-august-25-2026/";
